8076d78c969c7b48fa446ab605674a05b88abdcd
Minh Truong <tmtruong25@gmail.com>
2025-10-22 10:58:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['email'])) {
 -->
 <html>
     <head>
-        <title>Net net</title>
+        <title>Netnet</title>
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
         <script type="text/javascript" src="bootstrap/js/jquery-3.5.1.min.js"></script>
         <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
