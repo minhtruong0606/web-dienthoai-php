ca969f83c6db7230f44394fa6c22182307f7be98
Minh Truong <tmtruong25@gmail.com>
2025-10-29 09:48:59 +0700

    Update page title in contact.php

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
 -->
 <html>
     <head>
-        <title>E-Store|Contact Us</title>
+        <title>E-Store|Lien he</title>
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
         <script type="text/javascript" src="bootstrap/js/jquery-3.5.1.min.js"></script>
         <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
