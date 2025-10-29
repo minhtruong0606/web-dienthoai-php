784cd8814e8f181bb4bedcc4d04a4da87ceba0a5
Minh Truong <tmtruong25@gmail.com>
2025-10-29 10:25:19 +0700

    Update database connection parameters

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "", "ecommerce", 3306)
+    $con = mysqli_connect("172.31.24.242", "webuser", "Nt25102004", "ecommerce", 3306)
     or die(mysqli_connect_error());
     if (!isset($_SESSION)) {
         session_start();
